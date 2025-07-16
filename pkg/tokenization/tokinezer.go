080749b5794cb7869f4f8a50916a04fed68cbaba
Luca Calabria <luca.calabria@intel.com>
2025-07-16 18:13:30 +0200

    fix prints

--- pkg/tokenization/tokinezer.go
+++ pkg/tokenization/tokinezer.go
@@ -84,19 +84,19 @@ func NewCachedHFTokenizer(config *HFTokenizerConfig) (Tokenizer, error) {
 
 // Encode converts a string into token IDs.
 func (t *CachedHFTokenizer) Encode(input, modelName string) ([]uint32, []tokenizers.Offset, error) {
-	fmt.Println("Tokenizer.go:tokenization:Encode - Before t.cache.Get")
+	fmt.Print("Tokenizer.go:tokenization:Encode - Before t.cache.Get")
 	tk, ok := t.cache.Get(modelName)
-	fmt.Println("Tokenizer.go:tokenization:Encode - After t.cache.Get")
+	fmt.Print("Tokenizer.go:tokenization:Encode - After t.cache.Get")
 	if !ok {
-		fmt.Println("Tokenizer.go:tokenization:Encode - Before tokenizers.FromPretrained")
+		fmt.Print("Tokenizer.go:tokenization:Encode - Before tokenizers.FromPretrained")
 		tokenizer, err := tokenizers.FromPretrained(modelName, t.cfg)
 		if err != nil {
 			return nil, nil, err
 		}
-		fmt.Println("Tokenizer.go:tokenization:Encode - After tokenizers.FromPretrained")
-		fmt.Println("Tokenizer.go:tokenization:Encode - Before t.cache.Add")
+		fmt.Print("Tokenizer.go:tokenization:Encode - After tokenizers.FromPretrained")
+		fmt.Print("Tokenizer.go:tokenization:Encode - Before t.cache.Add")
 		t.cache.Add(modelName, tokenizer)
-		fmt.Println("Tokenizer.go:tokenization:Encode - After t.cache.Add")
+		fmt.Print("Tokenizer.go:tokenization:Encode - After t.cache.Add")
 		tk = tokenizer
 	}
 
@@ -104,9 +104,9 @@ func (t *CachedHFTokenizer) Encode(input, modelName string) ([]uint32, []tokeniz
 		tokenizers.WithReturnTypeIDs(),
 		tokenizers.WithReturnOffsets(),
 	}
-	fmt.Println("Tokenizer.go:tokenization:Encode - Before tk.EncodeWithOptions")
+	fmt.Print("Tokenizer.go:tokenization:Encode - Before tk.EncodeWithOptions")
 	resp := tk.EncodeWithOptions(input, true, encodeOptions...)
-	fmt.Println("Tokenizer.go:tokenization:Encode - After tk.EncodeWithOptions")
+	fmt.Print("Tokenizer.go:tokenization:Encode - After tk.EncodeWithOptions")
 	return resp.IDs, resp.Offsets, nil
 }
 
